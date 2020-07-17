1a7cb071fa26f0c99585a2f8a5a4b75bdf3185e4
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-17 10:52:32 +0530

    setup

---
+++ src/main/java/com/github/faizal/libraryeventskafka/LibraryEventsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.faizal.libraryeventskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/faizal/libraryeventskafka/LibraryEventsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.faizal.libraryeventskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
