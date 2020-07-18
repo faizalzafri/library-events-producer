5766a647c4c03c5306d08d4b20fd177f215a590b
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-19 00:56:47 +0530

    added controller integration test

---
+++ src/test/java/intg/com/github/faizal/libraryeventskafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.github.faizal.libraryeventskafka.controller;
+
+import com.github.faizal.libraryeventskafka.domain.Book;
+import com.github.faizal.libraryeventskafka.domain.LibraryEvent;
+import com.github.faizal.libraryeventskafka.domain.LibraryEventType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void postLibraryEvent() {
+        //given
+        Book book = Book.builder()
+                .bookId(101)
+                .bookName("Learn Kafka")
+                .bookAuthor("Faizal")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .libraryEventType(LibraryEventType.NEW)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity entity = new HttpEntity(libraryEvent, headers);
+
+        //when
+        ResponseEntity<LibraryEvent> response = restTemplate.exchange("/v1/libraryevent", HttpMethod.POST, entity, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void postLibraryEventSync() {
+        //given
+        Book book = Book.builder()
+                .bookId(101)
+                .bookName("Learn Kafka")
+                .bookAuthor("Faizal")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .libraryEventType(LibraryEventType.NEW)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity entity = new HttpEntity(libraryEvent, headers);
+
+        //when
+        ResponseEntity<LibraryEvent> response = restTemplate.exchange("/v1/libraryevent/sync", HttpMethod.POST, entity, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+}
