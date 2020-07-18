6b2ffde0ff531395a7e17910202fab45bfe214e2
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-18 17:25:27 +0530

    sending via sycn lambda and sync

--- src/main/java/com/github/faizal/libraryeventskafka/controller/LibraryEventsController.java
+++ src/main/java/com/github/faizal/libraryeventskafka/controller/LibraryEventsController.java
@@ -1,25 +1,40 @@
 package com.github.faizal.libraryeventskafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.faizal.libraryeventskafka.domain.LibraryEvent;
 import com.github.faizal.libraryeventskafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
     private LibraryEventProducer producer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
         // invoke kaka producer
-        producer.sendLibraryEvent(libraryEvent);
+        producer.sendLibraryEvent2(libraryEvent);
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PostMapping("/v1/libraryevent/sync")
+    public ResponseEntity<LibraryEvent> postLibraryEventSync(@RequestBody LibraryEvent libraryEvent) throws InterruptedException, ExecutionException, JsonProcessingException {
+
+        // invoke kaka producer
+        SendResult<Integer, String> result = producer.sendLibraryEventSync(libraryEvent);
+        log.info("Result: {}", result.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
--- src/main/java/com/github/faizal/libraryeventskafka/producer/LibraryEventProducer.java
+++ src/main/java/com/github/faizal/libraryeventskafka/producer/LibraryEventProducer.java
@@ -1,5 +1,6 @@
 package com.github.faizal.libraryeventskafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.faizal.libraryeventskafka.domain.LibraryEvent;
 import lombok.SneakyThrows;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -21,12 +24,9 @@ public class LibraryEventProducer {
     @Autowired
     private ObjectMapper mapper;
 
-    @SneakyThrows
-    public void sendLibraryEvent(LibraryEvent libraryEvent) {
-
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
         Integer key = libraryEvent.getLibraryEventId();
         String value = mapper.writeValueAsString(libraryEvent);
-
 //        batching and/or linger.ms in effect, thus returning future
         ListenableFuture<SendResult<Integer, String>> future = template.sendDefault(key, value);
         future.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
@@ -40,6 +40,29 @@ public class LibraryEventProducer {
                 handleSuccess(key, value, result);
             }
         });
+    }
+
+    public void sendLibraryEvent2(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = mapper.writeValueAsString(libraryEvent);
+        ListenableFuture<SendResult<Integer, String>> future = template.sendDefault(key, value);
+        future.addCallback(result -> handleSuccess(key, value, result), ex -> handleFailure(key, value, ex));
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSync(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = mapper.writeValueAsString(libraryEvent);
+        SendResult<Integer, String> result = null;
+        try {
+            result = template.sendDefault(key, value).get();
+        } catch (ExecutionException | InterruptedException ex) {
+            log.error("ExecutionException/InterruptedException. Not sent. Root cause: {}", ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            log.error("ExecutionException/InterruptedException. Not sent. Root cause: {}", ex.getMessage());
+            throw ex;
+        }
+        return result;
     }
 
     private void handleFailure(Integer key, String value, Throwable ex) {
