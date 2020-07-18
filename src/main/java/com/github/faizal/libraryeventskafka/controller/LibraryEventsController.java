c008877d1bfcc2054ada4e3872dd876ebe840e77
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-18 14:46:18 +0530

    sending to kafka, modified controller

--- src/main/java/com/github/faizal/libraryeventskafka/controller/LibraryEventsController.java
+++ src/main/java/com/github/faizal/libraryeventskafka/controller/LibraryEventsController.java
@@ -1,6 +1,8 @@
 package com.github.faizal.libraryeventskafka.controller;
 
 import com.github.faizal.libraryeventskafka.domain.LibraryEvent;
+import com.github.faizal.libraryeventskafka.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    private LibraryEventProducer producer;
+
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
 
         // invoke kaka producer
+        producer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
