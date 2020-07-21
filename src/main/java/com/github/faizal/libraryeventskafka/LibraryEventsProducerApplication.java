6e83b4942a587c756886424b02cb4d96ecb96914
Faizal <34762675+faizalzafri@users.noreply.github.com>
2020-07-21 14:04:15 +0530

    refactoring

--- src/main/java/com/github/faizal/libraryeventskafka/LibraryEventsProducerApplication.java
+++ src/main/java/com/github/faizal/libraryeventskafka/LibraryEventsProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryEventsKafkaApplication {
+public class LibraryEventsProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryEventsKafkaApplication.class, args);
+		SpringApplication.run(LibraryEventsProducerApplication.class, args);
 	}
 
 }
